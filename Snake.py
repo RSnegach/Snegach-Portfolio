a2d895f4c595e25c44b103228ab6d6ad35682791
RSnegach <snegach.r@northeastern.edu>
2023-10-26 16:39:57 -0400

    Finish snake

--- Snake.py
+++ Snake.py
@@ -1,5 +1,5 @@
 import pygame
-
+from random import randint
 # initialize game parameters (window, font, title, clock)
 pygame.init()
 window = pygame.display.set_mode((640,640))
@@ -15,6 +15,14 @@ class Node():
         self.next = next
         self.prev = prev
         self.direction = direction
+
+# the coin class representing coins which the snake must collect to grow
+class Coin():
+
+    def __init__(self, x_pos: int, y_pos:int, collected: bool):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.collected = collected
 
 # head node of snake
 head = Node(0,600,"right", None, None)
@@ -96,24 +104,88 @@ def collision_check(node):
         return
     elif node == head:
         if (head.y_pos > 640 or head.y_pos < 0) or (head.x_pos > 640 or head.x_pos < 0):
-            exit()
+            fps = 0
+            lose_message = game_font.render(f"Game Over! You Lose!", True, (255, 0, 0))
+            window.blit(lose_message,(240,150))
         else:
             collision_check(node.prev)
     elif node.prev == None and node.next != None:
         if (head.x_pos == node.x_pos) and (head.y_pos == node.y_pos):
-            exit()
+            fps = 0
+            lose_message = game_font.render(f"Game Over! You Win!", True, (255, 0, 0))
+            window.blit(lose_message,(240,150))
         else:
             return
     else:
         if (head.x_pos == node.x_pos) and (head.y_pos == node.y_pos):
-            exit()
+            fps = 0
+            lose_message = game_font.render(f"Game Over! You Win!", True, (255, 0, 0))
+            window.blit(lose_message,(240,150))
         else:
             collision_check(node.prev)
-#generate a coin in a random location
+# get the acceptable coordinate locations to spawn a coin (i.e, where snake is not)
+def get_coords(node):
+    if node.prev == None:
+        ref_coords.append([node.x_pos, node.y_pos])
+        return
+    else:
+        ref_coords.append([node.x_pos, node.y_pos])
+        get_coords(node.prev)
+        
+# list of coordinates where coin can spawn
+coords = []
+#list of coords where snake is occupying
+ref_coords = []
+
+for x in range(0,640,40):
+    for y in range(0,640,40):
+        coords.append([x,y])
+print(coords)
+
+# representation of coin which snake must collect
+coin = Coin(600,600, False)
+# spawn a coin in random location
+def spawn_coin():
+    global coords
+    global ref_coords
+    global coin
+
+    #coordinates not permitted if snake is occupying them
+    permitted_coords = list(filter(lambda x: x not in ref_coords, coords))
+    rand_index = randint(0, len(permitted_coords))
+    rand_coords = permitted_coords[rand_index]
+
+    coin.x_pos = rand_coords[0]
+    coin.y_pos = rand_coords[1]
+    
+# draw coin at random coordinates
+def draw_coin():
+    global coin
+    window.blit(coin_image,(coin.x_pos, coin.y_pos))
+# check if coin collected - if yes remove and spawn new
+def collect_check():
+    global game_score
+    if head.x_pos == coin.x_pos and head.y_pos == coin.y_pos:
+        coin.collected = True
+        add_tail(head)
+        game_score += 1
+    if game_score == 256:
+        fps = 0
+        win_message = game_font.render(f"Game Over! You Win!", True, (255, 0, 0))
+        window.blit(win_message,(240,150))
+
 #render image of a Node and a coin
 snakepiece = pygame.image.load("snake-piece.png")
-coin = pygame.image.load("coin.png")
+coin_image = pygame.image.load("coin.png")
+#game fps (3 by default, 6 when player presses space to speed up)
+fps = 3
+#set game score as 0
+game_score = 0
+# set game font
+game_font = pygame.font.SysFont("Arial", 24)
+pygame.display.set_caption("Snake")
 
+# game execution loop
 while True:
     # event handlers
     window.fill((0,0,0))
@@ -143,10 +215,26 @@ while True:
                     continue
                 head.direction = "down"
             if event.key == pygame.K_SPACE:
-                add_tail(head)
+                fps = 6
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                fps = 3
+    # move snake
     move(head)
-    draw_snake(head)
+    # check if snake collided - end game if yes
     collision_check(head)
+    # check if snake collected coin - grow snake and respawn coin if yes
+    collect_check()
+    # check new acceptable coords for coin spawn
+    get_coords(head)
+    #draw coin at coordinates
+    if coin.collected == True:
+        spawn_coin()
+        coin.collected = False
+    draw_coin()
+    # draw snake
+    draw_snake(head)
+    
     pygame.display.flip()
     # game FPS
-    clock.tick(3)
+    clock.tick(fps)
