6f79fdbdd205f88ecf8e5693b6f4f55418e21fe1
Roman Snegach <77998618+RSnegach@users.noreply.github.com>
2023-12-15 11:24:22 -0500

    Update Decision Tree.py

--- Football Results Prediction Model/Decision Tree.py
+++ Football Results Prediction Model/Decision Tree.py
@@ -8,15 +8,17 @@ from sklearn.tree import DecisionTreeRegressor
 # Path of the file to read
 file_path = 'Stats.csv'
 
+#dataframe consisting of statistical data from 'Stats.csv'
 data = pd.read_csv(file_path)
 
+# convert result data to int
 data.loc[data['Result'] == "W", 'Result'] = 1
 data.loc[data['Result'] == "D", 'Result'] = 0
 data.loc[data['Result'] == "L", 'Result'] = -1
 
-# Create target object and call it y
+# y represents what the model is predicting, i.e: Result, xG, GF, GA, etc.
 y = data.Gls
-# Create X
+# Create the feature space (which columns from the original data are used to create the model)
 features = ["xG"]
 X = data[features]
 
@@ -24,20 +26,19 @@ X = data[features]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
 # Specify Model
-iowa_model = DecisionTreeRegressor(random_state=1)
+model = DecisionTreeRegressor(random_state=1)
 # Fit Model
-iowa_model.fit(train_X, train_y)
+model.fit(train_X, train_y)
 
 # Make validation predictions and calculate mean absolute error
-val_predictions = iowa_model.predict(val_X)
+val_predictions = model.predict(val_X)
 val_mae = mean_absolute_error(val_predictions, val_y)
 print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
 
-# Using best value for max_leaf_nodes
-iowa_model = DecisionTreeRegressor(max_leaf_nodes=75, random_state=1)
-iowa_model.fit(train_X, train_y)
-val_predictions = iowa_model.predict(val_X)
-print(val_predictions)
-print(val_y)
-val_mae = mean_absolute_error(val_predictions, val_y)
-print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+# Using best value for max_leaf_nodes after determining what best value is (trial and error)
+# model.fit(train_X, train_y)
+# val_predictions = iowa_model.predict(val_X)
+# print(val_predictions)
+# print(val_y)
+# val_mae = mean_absolute_error(val_predictions, val_y)
+# print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
